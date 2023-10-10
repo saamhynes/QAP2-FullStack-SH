9e71d4bc4cba4d853da35e2e2f89b0cfc98846fc
saamhynes <127117184+saamhynes@users.noreply.github.com>
2023-10-10 14:18:20 -0230

    October 10/2023

---
+++ logger.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+const { EventEmitter } = require("events");
+
+const logDir = "./logs";
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+function getLogFileName() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  return path.join(logDir, `${year}-${month}-${day}.log`);
+}
+
+class MyLogger extends EventEmitter {
+  log(message) {
+    const logFileName = getLogFileName();
+    const logMessage = `[${new Date().toISOString()}] ${message}\n`;
+
+    // log to the console
+    console.log(logMessage);
+
+    // log to the file
+    fs.appendFile(logFileName, logMessage, (err) => {
+      if (err) {
+        console.log("Error writing to log file:", err);
+      }
+    });
+  }
+}
+
+const myLogger = new MyLogger();
+module.exports = MyLogger;
--- server.js
+++ server.js
@@ -1,80 +1,98 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
+const { EventEmitter } = require("events");
+const MyLogger = require("./logger");
+
+const myLogger = new MyLogger();
+
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
 
 const server = http.createServer((req, res) => {
   const url = req.url;
 
+  // Function to serve static files
+  const serveStaticFile = (fileName, contentType) => {
+    fs.readFile(fileName, "utf8", (err, data) => {
+      if (err) {
+        console.error(`Error serving ${fileName}: ${err.message}`);
+        myLogger.error(`Error serving ${fileName}: ${err.message}`);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+      console.log(`Served ${fileName} successfully.`);
+      myLogger.log(`Served ${fileName} successfully.`);
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    });
+  };
+
   switch (url) {
+    case "/":
+      // serve the home page
+      console.log("Home page requested.");
+      myLogger.log("Home page requested.");
+      serveStaticFile("Views/home.html", "text/html");
+      break;
+
     case "/about":
       // Read the about.html file from the views folder
       console.log("About page requested.");
-      fs.readFile("Views/about.html", "utf8", (err, data) => {
-        if (err) {
-          res.writeHead(500, { "Content-Type": "text/plain" });
-          res.end("Internal Server Error");
-          return;
-        }
-
-        // Serve the html content as the reponse
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
+      myLogger.log("About page requested.");
+      myEmitter.emit("routeAccessed", "/about");
+      serveStaticFile("Views/about.html", "text/html");
       break;
 
     case "/products":
       // Read the products.html file from the views folder
       console.log("Products page requested.");
-      fs.readFile("Views/products.html", "utf8", (err, data) => {
-        if (err) {
-          res.writeHead(500, { "Content-Type": "text/plain" });
-          res.end("Internal Server Error");
-          return;
-        }
-        // Serve the html content as the reponse
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
+      myLogger.log("Products page requested.");
+      myEmitter.emit("routeAccessed", "/products");
+      serveStaticFile("Views/products.html", "text/html");
       break;
 
     case "/contact":
       // Read the contact.html file from the views folder
       console.log("Contact page requested.");
-      fs.readFile("Views/contact.html", "utf8", (err, data) => {
-        if (err) {
-          res.writeHead(500, { "Content-Type": "text/plain" });
-          res.end("Internal Server Error");
-          return;
-        }
-        // Serve the html content as the reponse
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
+      myLogger.log("Contact page requested.");
+      myEmitter.emit("routeAccessed", "/contact");
+      serveStaticFile("Views/contact.html", "text/html");
       break;
 
     case "/subscribe":
       // Read the subscribe.html file from the views folder
       console.log("Subscribe page requested.");
-      fs.readFile("Views/subscribe.html", "utf8", (err, data) => {
-        if (err) {
-          res.writeHead(500, { "Content-Type": "text/plain" });
-          res.end("Internal Server Error");
-          return;
-        }
-        // Serve the html content as the reponse
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
+      myLogger.log("Subscribe page requested.");
+      myEmitter.emit("routeAccessed", "/subscribe");
+      serveStaticFile("Views/subscribe.html", "text/html");
+      break;
+
+    case "/stylesheet.css":
+      // Read the stylesheet.css file from the views folder
+      console.log("CSS file requested.");
+      myLogger.log("CSS file requested.");
+      serveStaticFile("Views/stylesheet.css", "text/css");
       break;
 
     default:
-      console.log("Page not found.");
+      console.log(`Page not found for URL: ${url}.`);
+      myLogger.error(`Page not found for URL: ${url}`);
+      myEmitter.emit("routeNotFound", url);
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end("Page not found");
   }
+});
+
+myEmitter.on("routeNotFound", (route) => {
+  console.error(`Route not found: ${route}`);
+  myLogger.error(`Route not found: ${route}`);
 });
 
 // set the srver to listen on port 3000
 const port = 3000;
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+  myLogger.log(`Server is running on http://localhost:${port}`);
 });
